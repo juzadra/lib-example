4336a67105891a9e080e36fbed0cd15db34977ac
juzadra <juliana_zadra@yahoo.com.br>
2025-07-05 13:43:12 -0300

    estrutura inicial biblioteca lib-example

---
+++ index.js
@@ -0,0 +1,36 @@
+const _ = require("lodash");
+
+const fullName = (name, surname) => {
+  return name + " " + surname;
+};
+
+function getMonthCurrent() {
+  const monthCurrent = new Date().getMonth();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  return months[monthCurrent];
+}
+
+const mult = (a, b) => {
+  return _.multiply(a, b);
+};
+
+module.exports = {
+  fullName,
+  getMonthCurrent,
+  mult,
+};
